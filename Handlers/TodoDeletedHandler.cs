05993ac33e288127509e18fc1a7afb99967602e8
Christian Gennari <christiangennari61@gmail.com>
2026-08-31 11:52:07 +0200

    feat(statistics): add deleted count tracking

--- Handlers/TodoDeletedHandler.cs
+++ Handlers/TodoDeletedHandler.cs
@@ -3,10 +3,12 @@ using arkitektur.Models.Events;
 
 namespace arkitektur.Handlers;
 
-public class TodoDeletedHandler(IActivityLogger logger) : IEventHandler<TodoDeleted>
+public class TodoDeletedHandler(IActivityLogger logger, IStatisticsService statistics)
+    : IEventHandler<TodoDeleted>
 {
     public async Task Handle(TodoDeleted @event)
     {
+        statistics.RecordDeleted();
         await logger.LogAsync("DELETE", $"Todo {@event.Todo.Id} raderad: {@event.Todo.Title}");
     }
 }
