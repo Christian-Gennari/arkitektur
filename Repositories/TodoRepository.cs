4b826b4899be61d348ec3e66bf0d4d43a540b06a
Christian Gennari <christiangennari61@gmail.com>
2026-08-31 10:45:55 +0200

    feat(todo repository): add CRUD methods to TodoRepository   - provide GetAll and GetById for reading todos   - implement Update to modify existing todo fields   - implement Delete to remove a todo from the list

--- Repositories/TodoRepository.cs
+++ Repositories/TodoRepository.cs
@@ -6,8 +6,39 @@ public class TodoRepository
 {
     private readonly List<Todo> todos = [];
 
+    #region Get methods
+    public List<Todo> GetAll()
+    {
+        return todos;
+    }
+
+    public Todo? GetById(int id)
+    {
+        return todos.FirstOrDefault(t => t.Id == id);
+    }
+
     public void Add(Todo todo)
     {
         todos.Add(todo);
     }
+
+    #endregion
+
+    #region Update and Delete Methods
+    public void Update(Todo todo)
+    {
+        var existingTodo = todos.FirstOrDefault(t => t.Id == todo.Id);
+        if (existingTodo != null)
+        {
+            existingTodo.Title = todo.Title;
+            existingTodo.IsCompleted = todo.IsCompleted;
+        }
+    }
+
+    public void Delete(Todo todo)
+    {
+        todos.Remove(todo);
+    }
+
+    #endregion
 }
